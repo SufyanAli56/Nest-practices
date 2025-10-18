0d44689a96c6dcc1f6963acc83304e2702088720
SufyanAli56 <sufyanalii1127@gmail.com>
2025-10-18 01:06:06 -0700

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return await user.save();
+  }
+
+  async findAll() {
+    return await this.userModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    return await this.userModel.findById(id).exec();
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+  }
+
+  async remove(id: string) {
+    return await this.userModel.findByIdAndDelete(id).exec();
+  }
+}
